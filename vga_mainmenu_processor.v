9639a4da2334be8c9668841eaa14afb3dcfe3ffb
gohuynh <ghh6@duke.edu>
2018-11-30 13:53:20 -0500

    just logo

--- vga_mainmenu_processor.v
+++ vga_mainmenu_processor.v
@@ -85,133 +85,133 @@ module vga_mainmenu_processor(curAddress,
 			addrToRead <= logo + logoOffset;
 		end
 		
-		// Option One (Play 1P) and Option Four (Top 1P)
-		else if (curY >= 10'd227 && curY < 10'd252)
-		begin
-			// P
-			if (curX >= 10'd133 && curX < 10'd154)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd133) + op1Offset;
-			// L
-			else if (curX >= 10'd154 && curX < 10'd175)
-				addrToRead <= zero + (19'd525 * 19'd21) + (curX - 10'd154) + op1Offset;
-			// A
-			else if (curX >= 10'd175 && curX < 10'd196)
-				addrToRead <= zero + (19'd525 * 19'd10) + (curX - 10'd175) + op1Offset;
-			// Y
-			else if (curX >= 10'd196 && curX < 10'd217)
-				addrToRead <= zero + (19'd525 * 19'd34) + (curX - 10'd196) + op1Offset;
-			// Space
-			else if (curX >= 10'd217 && curX < 10'd238)
-				addrToRead <= 19'd1923;
-			// 1
-			else if (curX >= 10'd238 && curX < 10'd259)
-				addrToRead <= zero + (19'd525 * 19'd1) + (curX - 10'd238) + op1Offset;
-			// P
-			else if (curX >= 10'd259 && curX < 10'd380)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd259) + op1Offset;
-			
-			
-			// T
-			else if (curX >= 10'd360 && curX < 10'd381)
-				addrToRead <= zero + (19'd525 * 19'd29) + (curX - 10'd360) + op1Offset;
-			// O
-			else if (curX >= 10'd381 && curX < 10'd402)
-				addrToRead <= zero + (19'd525 * 19'd24) + (curX - 10'd381) + op1Offset;
-			// P
-			else if (curX >= 10'd402 && curX < 10'd423)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd402) + op1Offset;
-			// Space
-			else if (curX >= 10'd423 && curX < 10'd444)
-				addrToRead <= 19'd1923;
-			// 1
-			else if (curX >= 10'd444 && curX < 10'd465)
-				addrToRead <= zero + (19'd525 * 19'd1) + (curX - 10'd444) + op1Offset;
-			// P
-			else if (curX >= 10'd465 && curX < 10'd486)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd465) + op1Offset;
-			// Space
-			else if (curX >= 10'd486 && curX < 10'd507)
-				addrToRead <= 19'd1923;
-			else
-				addrToRead <= 19'd1923;
-		end
-		// Option Two (Endless) and Option Five (Top end)
-		else if (curY >= 10'd285 && curY < 10'd247)
-		begin
-			// E
-			if (curX >= 10'd133 && curX < 10'd154)
-				addrToRead <= zero + (19'd525 * 19'd14) + (curX - 10'd133) + op2Offset;
-			// N
-			else if (curX >= 10'd154 && curX < 10'd175)
-				addrToRead <= zero + (19'd525 * 19'd23) + (curX - 10'd154) + op2Offset;
-			// D
-			else if (curX >= 10'd175 && curX < 10'd196)
-				addrToRead <= zero + (19'd525 * 19'd13) + (curX - 10'd175) + op2Offset;
-			// L
-			else if (curX >= 10'd196 && curX < 10'd217)
-				addrToRead <= zero + (19'd525 * 19'd21) + (curX - 10'd196) + op2Offset;
-			// E
-			else if (curX >= 10'd217 && curX < 10'd238)
-				addrToRead <= zero + (19'd525 * 19'd14) + (curX - 10'd217) + op2Offset;
-			// S
-			else if (curX >= 10'd238 && curX < 10'd259)
-				addrToRead <= zero + (19'd525 * 19'd28) + (curX - 10'd238) + op2Offset;
-			// S
-			else if (curX >= 10'd259 && curX < 10'd380)
-				addrToRead <= zero + (19'd525 * 19'd28) + (curX - 10'd259) + op2Offset;
-			
-			
-			// T
-			else if (curX >= 10'd360 && curX < 10'd381)
-				addrToRead <= zero + (19'd525 * 19'd29) + (curX - 10'd360) + op2Offset;
-			// O
-			else if (curX >= 10'd381 && curX < 10'd402)
-				addrToRead <= zero + (19'd525 * 19'd24) + (curX - 10'd381) + op2Offset;
-			// P
-			else if (curX >= 10'd402 && curX < 10'd423)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd402) + op2Offset;
-			// Space
-			else if (curX >= 10'd423 && curX < 10'd444)
-				addrToRead <= 19'd1923;
-			// E
-			else if (curX >= 10'd444 && curX < 10'd465)
-				addrToRead <= zero + (19'd525 * 19'd14) + (curX - 10'd444) + op2Offset;
-			// N
-			else if (curX >= 10'd465 && curX < 10'd486)
-				addrToRead <= zero + (19'd525 * 19'd23) + (curX - 10'd465) + op2Offset;
-			// D
-			else if (curX >= 10'd486 && curX < 10'd507)
-				addrToRead <= zero + (19'd525 * 19'd13) + (curX - 10'd486) + op2Offset;
-			else
-				addrToRead <= 19'd1923;
-		end
-		// Option Three (Play 2P)
-		else if (curY >= 10'd343 && curY < 10'd368)
-		begin
-			// P
-			if (curX >= 10'd133 && curX < 10'd154)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd133) + op3Offset;
-			// L
-			else if (curX >= 10'd154 && curX < 10'd175)
-				addrToRead <= zero + (19'd525 * 19'd21) + (curX - 10'd154) + op3Offset;
-			// A
-			else if (curX >= 10'd175 && curX < 10'd196)
-				addrToRead <= zero + (19'd525 * 19'd10) + (curX - 10'd175) + op3Offset;
-			// Y
-			else if (curX >= 10'd196 && curX < 10'd217)
-				addrToRead <= zero + (19'd525 * 19'd34) + (curX - 10'd196) + op3Offset;
-			// Space
-			else if (curX >= 10'd217 && curX < 10'd238)
-				addrToRead <= 19'd1923;
-			// 1
-			else if (curX >= 10'd238 && curX < 10'd259)
-				addrToRead <= zero + (19'd525 * 19'd2) + (curX - 10'd238) + op3Offset;
-			// P
-			else if (curX >= 10'd259 && curX < 10'd380)
-				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd259) + op3Offset;
-			else
-				addrToRead <= 19'd1923;
-		end
+//		// Option One (Play 1P) and Option Four (Top 1P)
+//		else if (curY >= 10'd227 && curY < 10'd252)
+//		begin
+//			// P
+//			if (curX >= 10'd133 && curX < 10'd154)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd133) + op1Offset;
+//			// L
+//			else if (curX >= 10'd154 && curX < 10'd175)
+//				addrToRead <= zero + (19'd525 * 19'd21) + (curX - 10'd154) + op1Offset;
+//			// A
+//			else if (curX >= 10'd175 && curX < 10'd196)
+//				addrToRead <= zero + (19'd525 * 19'd10) + (curX - 10'd175) + op1Offset;
+//			// Y
+//			else if (curX >= 10'd196 && curX < 10'd217)
+//				addrToRead <= zero + (19'd525 * 19'd34) + (curX - 10'd196) + op1Offset;
+//			// Space
+//			else if (curX >= 10'd217 && curX < 10'd238)
+//				addrToRead <= 19'd1923;
+//			// 1
+//			else if (curX >= 10'd238 && curX < 10'd259)
+//				addrToRead <= zero + (19'd525 * 19'd1) + (curX - 10'd238) + op1Offset;
+//			// P
+//			else if (curX >= 10'd259 && curX < 10'd380)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd259) + op1Offset;
+//			
+//			
+//			// T
+//			else if (curX >= 10'd360 && curX < 10'd381)
+//				addrToRead <= zero + (19'd525 * 19'd29) + (curX - 10'd360) + op1Offset;
+//			// O
+//			else if (curX >= 10'd381 && curX < 10'd402)
+//				addrToRead <= zero + (19'd525 * 19'd24) + (curX - 10'd381) + op1Offset;
+//			// P
+//			else if (curX >= 10'd402 && curX < 10'd423)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd402) + op1Offset;
+//			// Space
+//			else if (curX >= 10'd423 && curX < 10'd444)
+//				addrToRead <= 19'd1923;
+//			// 1
+//			else if (curX >= 10'd444 && curX < 10'd465)
+//				addrToRead <= zero + (19'd525 * 19'd1) + (curX - 10'd444) + op1Offset;
+//			// P
+//			else if (curX >= 10'd465 && curX < 10'd486)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd465) + op1Offset;
+//			// Space
+//			else if (curX >= 10'd486 && curX < 10'd507)
+//				addrToRead <= 19'd1923;
+//			else
+//				addrToRead <= 19'd1923;
+//		end
+//		// Option Two (Endless) and Option Five (Top end)
+//		else if (curY >= 10'd285 && curY < 10'd247)
+//		begin
+//			// E
+//			if (curX >= 10'd133 && curX < 10'd154)
+//				addrToRead <= zero + (19'd525 * 19'd14) + (curX - 10'd133) + op2Offset;
+//			// N
+//			else if (curX >= 10'd154 && curX < 10'd175)
+//				addrToRead <= zero + (19'd525 * 19'd23) + (curX - 10'd154) + op2Offset;
+//			// D
+//			else if (curX >= 10'd175 && curX < 10'd196)
+//				addrToRead <= zero + (19'd525 * 19'd13) + (curX - 10'd175) + op2Offset;
+//			// L
+//			else if (curX >= 10'd196 && curX < 10'd217)
+//				addrToRead <= zero + (19'd525 * 19'd21) + (curX - 10'd196) + op2Offset;
+//			// E
+//			else if (curX >= 10'd217 && curX < 10'd238)
+//				addrToRead <= zero + (19'd525 * 19'd14) + (curX - 10'd217) + op2Offset;
+//			// S
+//			else if (curX >= 10'd238 && curX < 10'd259)
+//				addrToRead <= zero + (19'd525 * 19'd28) + (curX - 10'd238) + op2Offset;
+//			// S
+//			else if (curX >= 10'd259 && curX < 10'd380)
+//				addrToRead <= zero + (19'd525 * 19'd28) + (curX - 10'd259) + op2Offset;
+//			
+//			
+//			// T
+//			else if (curX >= 10'd360 && curX < 10'd381)
+//				addrToRead <= zero + (19'd525 * 19'd29) + (curX - 10'd360) + op2Offset;
+//			// O
+//			else if (curX >= 10'd381 && curX < 10'd402)
+//				addrToRead <= zero + (19'd525 * 19'd24) + (curX - 10'd381) + op2Offset;
+//			// P
+//			else if (curX >= 10'd402 && curX < 10'd423)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd402) + op2Offset;
+//			// Space
+//			else if (curX >= 10'd423 && curX < 10'd444)
+//				addrToRead <= 19'd1923;
+//			// E
+//			else if (curX >= 10'd444 && curX < 10'd465)
+//				addrToRead <= zero + (19'd525 * 19'd14) + (curX - 10'd444) + op2Offset;
+//			// N
+//			else if (curX >= 10'd465 && curX < 10'd486)
+//				addrToRead <= zero + (19'd525 * 19'd23) + (curX - 10'd465) + op2Offset;
+//			// D
+//			else if (curX >= 10'd486 && curX < 10'd507)
+//				addrToRead <= zero + (19'd525 * 19'd13) + (curX - 10'd486) + op2Offset;
+//			else
+//				addrToRead <= 19'd1923;
+//		end
+//		// Option Three (Play 2P)
+//		else if (curY >= 10'd343 && curY < 10'd368)
+//		begin
+//			// P
+//			if (curX >= 10'd133 && curX < 10'd154)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd133) + op3Offset;
+//			// L
+//			else if (curX >= 10'd154 && curX < 10'd175)
+//				addrToRead <= zero + (19'd525 * 19'd21) + (curX - 10'd154) + op3Offset;
+//			// A
+//			else if (curX >= 10'd175 && curX < 10'd196)
+//				addrToRead <= zero + (19'd525 * 19'd10) + (curX - 10'd175) + op3Offset;
+//			// Y
+//			else if (curX >= 10'd196 && curX < 10'd217)
+//				addrToRead <= zero + (19'd525 * 19'd34) + (curX - 10'd196) + op3Offset;
+//			// Space
+//			else if (curX >= 10'd217 && curX < 10'd238)
+//				addrToRead <= 19'd1923;
+//			// 1
+//			else if (curX >= 10'd238 && curX < 10'd259)
+//				addrToRead <= zero + (19'd525 * 19'd2) + (curX - 10'd238) + op3Offset;
+//			// P
+//			else if (curX >= 10'd259 && curX < 10'd380)
+//				addrToRead <= zero + (19'd525 * 19'd25) + (curX - 10'd259) + op3Offset;
+//			else
+//				addrToRead <= 19'd1923;
+//		end
 		else
 			addrToRead <= 19'd1923;
 	end
